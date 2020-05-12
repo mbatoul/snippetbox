1d895f91d39533e4d31869542c34a2705c82b544
mbatoul <batoulmathis@gmail.com>
2020-05-12 09:24:21 +0200

    WIP

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # Snippetbox
 
+https://gist.github.com/
+
 ## Project Structure
-
-
 
 - `cmd` contains the application-specific code for the executables of the project
 - `pkg` contains the ancillary non-application-specific-code like validation helpers or SQL database models
 - `ui` contains the user interface assets (HTML templates and static files like CSS or images)
+
+## Tools and librairies
+
+- https://ace.c9.io
+
+## Learning materials
+
+- https://deviq.com/category/principles/
--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,17 +4,37 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"text/template"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Hello from Snippetbox"))
+
+	files := []string{
+		"ui/html/home.page.tmpl",
+		"ui/html/base.layout.tmpl",
+		"ui/html/footer.partial.tmpl",
+	}
+
+	// ts stands for template set
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.errorLog.Println(err.Error())
+		http.Error(w, "Inernal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		app.errorLog.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -23,7 +43,7 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", 405)
