67b66464fe6c5ce4526c4298c886b09e2bbc1d0a
mbatoul <batoulmathis@gmail.com>
2020-05-19 23:37:31 +0200

    Convert tabs to spaces

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -14,9 +14,9 @@ type SnippetModel struct {
 
 func (m *SnippetModel) Insert(title, content string, expires string) (int, error) {
 	stmt := `
-		INSERT INTO snippets (title, content, created, expires)
-		VALUES (?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))
-	`
+    INSERT INTO snippets (title, content, created, expires)
+    VALUES (?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))
+  `
 
 	result, err := m.DB.Exec(stmt, title, content, expires)
 	if err != nil {
@@ -33,10 +33,10 @@ func (m *SnippetModel) Insert(title, content string, expires string) (int, error
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
 	stmt := `
-		SELECT * FROM snippets
-		WHERE expires > UTC_TIMESTAMP()
-		AND id = ?
-	`
+    SELECT * FROM snippets
+    WHERE expires > UTC_TIMESTAMP()
+    AND id = ?
+  `
 
 	s := &models.Snippet{}
 	err := m.DB.QueryRow(stmt, id).Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Created)
